614aa5fa0570b741b57cbde99b2fd40859b9a5b9
David Reinfurt <reinfurt@o-r-g.com>
2014-12-31 12:32:29 -0600

    Add "add to homepage" instructions

--- index.php
+++ index.php
@@ -5,21 +5,14 @@
         <title>Multi</title>
         <meta http-equiv="Content-Type" content="text/xhtml; charset=utf-8" />
         <meta http-equiv="Title" content="Multi" />
-
 	<!-- <meta name="viewport" content="user-scalable=no, width=200" /> -->
-
-	<!-- <meta name="viewport" content="width=400, initial-scale=10.0" /> -->
-
-	<!-- <meta name=viewport content="width=device-width, initial-scale=10.0"> -->
-
 	<meta name = "viewport" content = "user-scalable=no, width=device-width">
-
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 	<link rel="apple-touch-icon" href="_Resources/apple-touch-icon.png" />
-        <!-- <script src="_Processing/processing.min.js"></script> -->
-        <script src="_Processing/processing-1.3.6.js"></script>
-        <script src="_Resources/minim.js"></script>
+        <script src="_Processing/processing.min.js"></script>
+        <!-- <script src="_Processing/processing-1.3.6.js"></script> -->
+        <!-- <script src="_Resources/minim.js"></script> -->
 
 	<script>
 		function BlockMove(event) {
@@ -34,13 +27,15 @@
         		-webkit-touch-callout: none;
 	        	/* -webkit-user-select: none; */
     			}
-
-/*
-// this is for mobile from Modern Art
-/* Mobile Styles */
-@media screen and (max-width: 568px) {
-}
-*/
+		
+		#addtohome {
+			font-family: Helvetica, sans-serif; 
+			font-size: 72px; 
+			width: 568px; 
+			height: 1163px;
+			padding: 100px; 
+			background-color:#FFF;
+		}
 	-->
 	</style>
 </head>
@@ -53,12 +48,13 @@
 
 		temp = document.getElementById('Multi');
 	
- 		if (window.navigator.standalone) {
+  		if (!window.navigator.standalone) {
 
 			// fill in correct css to center message and show icon
 
 			// temp.innerHTML = '<canvas datasrc="_Processing/Multi.pde"></canvas>';	
-			temp.innerHTML = '<div style="font-family: Helvetica, sans-serif; font-size: 14px; width: 688px; padding:40px; background-color:#FFF;">Click below to install Multi, choose Add to home screen ...</div>';
+			// temp.innerHTML = '<div style="font-family: Helvetica, sans-serif; font-size: 14px; width: 688px; padding:40px; background-color:#FFF;">Click below to install Multi, choose Add to home screen ...</div>';
+			temp.innerHTML = '<div id="addtohome">To install Multi, please click below and choose Add to home screen ... </div>';
 
 		} else {
 
